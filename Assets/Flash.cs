5e2e71ce0250471bd623e7ca11cd00156c480c33
蕭文琳 <llynn5263tw@gmail.com>
2018-05-18 12:02:28 +0800

    0518

---
+++ Assets/Flash.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flash : MonoBehaviour
+{
+
+    public new Light light;    //assign gameobject with light component attached
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {      //Left mouse button
+            light.enabled = !light.enabled;      //changes light on/off
+        }
+    }
+}
